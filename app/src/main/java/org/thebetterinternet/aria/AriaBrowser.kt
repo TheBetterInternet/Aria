5b95d1ce193d511fb80e1c516c272f1fcadfae0a
NEOAPPS <asd22.info@gmail.com>
2025-05-29 22:50:32 +0300

    refactor: modularize everything in a file, and more

--- app/src/main/java/org/thebetterinternet/aria/AriaBrowser.kt
+++ app/src/main/java/org/thebetterinternet/aria/AriaBrowser.kt
@@ -31,10 +31,12 @@ import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.TransformOrigin
